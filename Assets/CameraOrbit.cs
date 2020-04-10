d78b821313a9bdf9dfbdcca4fe7b962c3570b599
Adrian Biagioli <adrianbiagioli@gmail.com>
2020-04-10 16:36:07 -0400

    Include Terrain test scene, CameraOrbit script

---
+++ Assets/CameraOrbit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{
+    private void Update()
+    {
+        var hoz = Input.GetAxis("Horizontal");
+        var vrt = Input.GetAxis("Vertical");
+        var look_hoz = Input.GetAxis("Mouse X");
+        var look_vrt = Input.GetAxis("Mouse Y");
+
+        var t = transform;
+        var pos = t.position;
+
+        var fwd = t.forward;
+        var rgt = t.right;
+        fwd = Quaternion.AngleAxis(-look_vrt * 20.0f, rgt) * fwd;
+        var up = Vector3.Cross(rgt, fwd);
+        fwd = Quaternion.AngleAxis(-look_hoz * 20.0f, up) * fwd;
+        t.rotation = Quaternion.LookRotation(fwd, Vector3.up);
+        
+
+        pos += t.right * (hoz * Time.deltaTime * 300.0f);
+        pos += t.forward * (vrt * Time.deltaTime * 300.0f);
+        
+        t.position = pos;
+    }
+}
